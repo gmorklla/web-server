25c8fe8cf1627bf7b37d01a1fbcd873c20da88b0
Alejandro RV <gmorklla@gmail.com>
2015-11-09 15:01:27 -0600

    Inicia repo

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var PUERTO = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('Ruta privada');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log('Request: ' + new Date().toString() + ' | ' + req.method + ' ' + req.originalUrl );
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PUERTO, function () {
+	console.log('Servidor Express ha comensado en el puerto: ' + PUERTO);
+});
